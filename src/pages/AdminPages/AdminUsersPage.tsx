7d5eabdca1868181586b068603171859603171f1
aren talb <aren.talb00@gmail.com>
2024-06-11 21:43:48 +0300

    Split AdminUsersPage to multiple component

--- src/pages/AdminPages/AdminUsersPage.tsx
+++ src/pages/AdminPages/AdminUsersPage.tsx
@@ -1,70 +1,10 @@
-import { useGetUsersQuery } from "../../redux/user/userApiSlice.ts";
-import { toast } from "react-toastify";
-import { TApiError } from "../../types/TApiError.ts";
-import { FiCheck, FiX } from "react-icons/fi";
-import { Loader } from "../../components/common/Loader.tsx";
+import { UserList } from "../../components/admin/UserList";
 
 export function AdminUsersPage() {
   return (
     <div className="py-10">
       <h1 className="text-3xl font-bold mb-8">All Users</h1>
       <UserList />
-    </div>
-  );
-}
-
-export function UserList() {
-  const {
-    data: usersResponse,
-    isLoading: isUsersLoading,
-    error: usersError,
-    isError: isUsersError,
-  } = useGetUsersQuery();
-
-  if (isUsersError && usersError) {
-    const apiError = usersError as TApiError;
-    toast.error(apiError?.data?.message || "An error occurred");
-    return <div>Error loading users</div>;
-  }
-
-  if (isUsersLoading) {
-    return <Loader />;
-  }
-
-  if (!usersResponse || !usersResponse.data) {
-    return <div>No users found</div>;
-  }
-
-  const users = usersResponse.data;
-
-  return (
-    <div className="overflow-x-auto">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Email</th>
-            <th>Role</th>
-            <th>Verified</th>
-            <th>Active</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user, index) => (
-            <tr key={user._id}>
-              <td>{index + 1}</td>
-              <td>{user._id}</td>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.role}</td>
-              <td>{user.isVerified ? <FiCheck /> : <FiX />}</td>
-              <td>{user.active ? <FiCheck /> : <FiX />}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
     </div>
   );
 }
