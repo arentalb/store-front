cfbf2d7aaecde4664d6c82dca91f97a0477a8537
aren talb <aren.talb00@gmail.com>
2024-06-13 03:50:41 +0300

    Refactor UserProductsPage to use better state handling

--- src/pages/UserPages/UserProductsPage.tsx
+++ src/pages/UserPages/UserProductsPage.tsx
@@ -1,53 +1,44 @@
-// src/pages/UserPages/UserProductsPage.tsx
-
 import { TProduct } from "../../types/TProduct.ts";
-import { Link, useLocation } from "react-router-dom";
 import { TApiError } from "../../types/TApiError.ts";
 import { useGetProductsQuery } from "../../redux/product/productApiSlice.ts";
 import { Loader } from "../../components/common/Loader.tsx";
 import { Product } from "../../components/user/Product";
+import { ErrorMessage } from "../../components/common/ErrorMessage.tsx";
+import { EmptyMessage } from "../../components/common/EmptyMessage.tsx";
 
 export function UserProductsPage() {
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const category = searchParams.get("category");
+  const {
+    data: ProductsResponse,
+    isLoading: isProductsLoading,
+    error: productsError,
+    isError: isProductsError,
+  } = useGetProductsQuery();
+  const products: TProduct[] | undefined = ProductsResponse?.data || [];
 
-  const { data, isLoading, error, isError } = useGetProductsQuery();
-  const products: TProduct[] | undefined = data?.data;
-  const errorMessage = (error as TApiError)?.data?.message;
-
+  if (isProductsError) {
+    const apiError = productsError as TApiError;
+    return (
+      <ErrorMessage
+        message={
+          apiError.data?.message || "An error occurred while fetching products"
+        }
+      />
+    );
+  }
+  if (products?.length === 0) {
+    return <EmptyMessage message={"There is no product to show "} />;
+  }
+  if (isProductsLoading) return <Loader />;
   return (
     <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <>
-          {isError ? (
-            <div className="flex flex-col items-center">
-              <p className="text-center text-xl mb-8">{errorMessage}</p>
-              <Link to="/home" className="btn btn-primary">
-                Return home
-              </Link>
-            </div>
-          ) : (
-            ""
-          )}
-          {products && (
-            <>
-              <h1 className="uppercase text-3xl font-bold mb-6">
-                {`ALL ${category ? category : ""} PRODUCTS`}
-              </h1>
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-12">
-                {products.map((pro) => (
-                  <div key={pro._id} className="flex-shrink-0 w-full md:w-auto">
-                    <Product pro={pro} />
-                  </div>
-                ))}
-              </div>
-            </>
-          )}
-        </>
-      )}
+      <h1 className="uppercase text-3xl font-bold mb-6">ALL PRODUCTS</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 mb-12">
+        {products.map((pro) => (
+          <div key={pro._id} className="flex-shrink-0 w-full md:w-auto">
+            <Product pro={pro} />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
