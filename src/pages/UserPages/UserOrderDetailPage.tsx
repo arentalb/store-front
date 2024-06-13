fe92cc18a8bbd4c3b7e999807c52bd71131bb35c
aren talb <aren.talb00@gmail.com>
2024-06-13 04:14:03 +0300

    Refactor UserOrderDetailPage to use better state handling

--- src/pages/UserPages/UserOrderDetailPage.tsx
+++ src/pages/UserPages/UserOrderDetailPage.tsx
@@ -1,25 +1,39 @@
 import { useParams } from "react-router-dom";
-import { toast } from "react-toastify";
 import { useGetUserOrderDetailQuery } from "../../redux/order/orderApiSlice.ts";
 import { TApiError } from "../../types/TApiError.ts";
 import { TOrder } from "../../types/TOrder.ts";
 import { Loader } from "../../components/common/Loader.tsx";
 import { OrderDetailsTable } from "../../components/user/OrderDetailsTable.tsx";
 import { OrderItemsTable } from "../../components/user/OrderItemsTable.tsx";
+import { ErrorMessage } from "../../components/common/ErrorMessage.tsx";
 
 export function UserOrderDetailPage() {
   const { id } = useParams<{ id: string }>();
-  const { data, error, isLoading } = useGetUserOrderDetailQuery(id!);
+  const {
+    data: orderResponse,
+    error: orderError,
+    isLoading: isOrderLoading,
+    isError: isOrderError,
+  } = useGetUserOrderDetailQuery(id!);
 
-  const order: TOrder | undefined = data?.data;
+  const order: TOrder | undefined = orderResponse?.data;
 
-  if (isLoading) return <Loader />;
-  if (error) {
-    const apiError = error as TApiError;
-    toast.error(apiError?.data?.message || "An error occurred");
-    return <div>Error loading order details</div>;
+  if (isOrderLoading) return <Loader />;
+
+  if (isOrderError) {
+    const apiError = orderError as TApiError;
+    return (
+      <ErrorMessage
+        message={
+          apiError.data?.message || "An error occurred while fetching order"
+        }
+      />
+    );
   }
 
+  if (!order) {
+    return <ErrorMessage message={"No order available "} />;
+  }
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-6">Order Details</h1>
