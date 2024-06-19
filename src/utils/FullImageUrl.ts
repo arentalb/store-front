e0c22af74fc846b2df6be3e1ed7e90fa88e6a35b
aren talb <aren.talb00@gmail.com>
2024-06-19 17:30:13 +0300

    Fix t=not showing image preview when uploaded from local machine

--- src/utils/FullImageUrl.ts
+++ src/utils/FullImageUrl.ts
@@ -1,4 +1,10 @@
 export function FullImageUrl(imageName: string) {
   const baseURL = import.meta.env.VITE_IMAGE_BASE_URL;
+
+  // Check if the imageName is a local preview URL
+  if (imageName.startsWith("blob:")) {
+    return imageName;
+  }
+
   return `${baseURL}${imageName}`;
 }
